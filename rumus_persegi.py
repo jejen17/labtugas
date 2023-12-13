f64e8995f9da2da948c131ee6723332937366d67
jejen17 <153716639+jejen17@users.noreply.github.com>
2023-12-13 15:55:34 +0700

    Add files via upload

---
+++ rumus_persegi.py
@@ -0,0 +1,9 @@
+p = int(input("Masukan Panjang : "))
+l = int(input("Masukan Lebar  : "))
+
+luas = p * l
+
+print("Panjang : ", p)
+print("Lebar   : ", l)
+print("Luas = Panjang x Lebar")
+print("Luas    : ", luas)
