42123f4e5039007df62d8881f6ffe272ebec45b3
jejen17 <153716639+jejen17@users.noreply.github.com>
2023-12-14 08:20:10 +0700

    Rumus_Persegi.py

--- rumus_persegi.py
+++ rumus_persegi.py
@@ -2,8 +2,12 @@ p = int(input("Masukan Panjang : "))
 l = int(input("Masukan Lebar  : "))
 
 luas = p * l
+k = 2*(p+l)
 
 print("Panjang : ", p)
 print("Lebar   : ", l)
 print("Luas = Panjang x Lebar")
 print("Luas    : ", luas)
+print("\n")
+print("Keliling Persegi Panjang = 2x(Panjang+Lebar)")
+print("Kelilig Persegi Panjang : ",k)
